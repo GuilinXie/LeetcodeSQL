b81c40662933c715965319457189ed2223636ccf
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-04-06 11:04:55 -0700

    Add files via upload

---
+++ SQL/178_Rand_Scores.py
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT s1.Score, COUNT(s2.Score) AS Rank
+FROM Scores s1, (SELECT DISTINCT Score FROM Scores) s2
+WHERE s1.Score<=s2.Score
+GROUP BY s1.Id
+ORDER BY s1.Score DESC
