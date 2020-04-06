e4fd527c0fb654a2bb3acb7f7bad570efce6f063
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-04-06 10:26:31 -0700

    Add files via upload

---
+++ SQL/262_Trips_and_Users.py
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+SELECT t.Request_at AS Day, ROUND(SUM(cancel) / sum(total), 2) AS "Cancellation Rate"
+
+FROM
+(SELECT t1.Request_at,
+ CASE WHEN t1.Status="cancelled_by_driver" THEN 1 WHEN t1.status="cancelled_by_client" THEN 1 ELSE 0 END AS cancel,
+ 1 AS total
+
+ FROM Trips t1
+
+ WHERE t1.Client_Id IN
+ (SELECT DISTINCT Users_Id
+ FROM Users u
+ WHERE u.Banned="No")
+
+AND
+t1.Driver_Id IN
+(SELECT DISTINCT Users_Id
+FROM Users u
+WHERE u.Banned = "No")
+
+AND t1.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+) t
+GROUP BY t.Request_at
