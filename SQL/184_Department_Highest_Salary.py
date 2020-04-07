2bbe8bfafc63f2d366257fe3dd576457c521d859
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-04-07 15:47:20 -0700

    Add files via upload

---
+++ SQL/184_Department_Highest_Salary.py
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+# Method 1 - beat 50%, similar to 185, Department top3
+SELECT d.Name AS Department, e1.Name AS Employee, e1.Salary
+FROM Employee e1
+JOIN Department d
+ON e1.DepartmentId=d.Id
+WHERE 1=(SELECT COUNT(e2.Salary)
+         FROM (SELECT DISTINCT DepartmentId, Salary FROM Employee) e2
+         WHERE e1.DepartmentId=e2.DepartmentId AND e1.Salary<=e2.Salary
+        )
+
+
+# Method 2 - beat 98% - more direct using max for the highest
+SELECT D.Name AS Department, E.Name AS Employee, E.Salary
+FROM Employee E
+INNER JOIN Department D
+ON E.DepartmentId=D.Id
+WHERE (Salary, DepartmentId) IN
+(SELECT MAX(Salary) AS Salary, DepartmentId
+FROM Employee
+GROUP BY DepartmentId)
