09281846d48cd5a06373aaf29ded999c98054c18
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-05-14 16:32:38 -0700

    Update 176_Sencond_Highest_Salary.py

--- SQL/176_Sencond_Highest_Salary.py
+++ SQL/176_Sencond_Highest_Salary.py
@@ -7,7 +7,17 @@ SELECT NULL
 ORDER BY SecondHighestSalary DESC
 LIMIT 1,1
 
-# Method2 - beat 50%, sometimes beat 90%...
+# Method 2 - RANK() window function - beat 10%, slower but suitable for Nth salary
+# Use MAX to return null if not exits.
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM (
+    SELECT Salary, RANK() OVER (ORDER BY Salary DESC) RN
+    FROM Employee
+) A
+WHERE RN = 2
+
+
+# Method3 - beat 50%, sometimes beat 90%..., but not suitable for Nth salary
 # max() will return NULL, if not exist
 SELECT max(Salary) as SecondHighestSalary
 FROM Employee
