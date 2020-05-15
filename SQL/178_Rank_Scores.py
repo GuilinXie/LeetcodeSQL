8df16d41bf3f126241403ff352a56a4cf07fc7c4
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-05-14 17:10:07 -0700

    Update 178_Rank_Scores.py

--- SQL/178_Rank_Scores.py
+++ SQL/178_Rank_Scores.py
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
 
+# Method 1 - beat 10%, sometimes 70% - DENSE_RANK()
+SELECT Score, DENSE_RANK() OVER (ORDER BY Score DESC) AS "Rank"
+FROM Scores
+
+# Method 2 - too complicate to remember....
 SELECT s1.Score, COUNT(s2.Score) AS Rank
 FROM Scores s1, (SELECT DISTINCT Score FROM Scores) s2
 WHERE s1.Score<=s2.Score
