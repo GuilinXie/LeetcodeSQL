db5d76a68fbbc1e964d88191d0007804a893bdfa
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-08-10 12:06:08 -0700

    Update 185_Department_Top_Three_Salaries.py

--- SQL/185_Department_Top_Three_Salaries.py
+++ SQL/185_Department_Top_Three_Salaries.py
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
 
+# Solution 0 - window function - easier to understand
+# beat 61%
+SELECT t.Department, t.Employee, t.Salary
+FROM 
+(SELECT d.Name AS Department, e.Name AS Employee, e.Salary AS Salary, DENSE_RANK() OVER(PARTITION BY e.DepartmentId ORDER BY e.Salary DESC) AS rk
+FROM Employee e
+INNER JOIN Department d   # inner join means if the departmentId is not find, then drop this rows, if use LEFT JOIN, then will keep it with DepartmentId and DepartmentName as NULL
+ON e.DepartmentId=d.Id) t
+WHERE t.rk <= 3
+
+
 # Solution 1 - Record my own, beat 98%
 SELECT d.Name AS Department, e1.Name AS Employee, e1.Salary
 FROM Employee e1
