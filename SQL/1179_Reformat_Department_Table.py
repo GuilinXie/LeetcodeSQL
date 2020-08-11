a7615d2d7b6cd1b51c3f4a5b5aea6fe6f582adcc
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-08-11 11:47:51 -0700

    Update 1179_Reformat_Department_Table.py

--- SQL/1179_Reformat_Department_Table.py
+++ SQL/1179_Reformat_Department_Table.py
@@ -1,5 +1,27 @@
 # Write your MySQL query statement below
 
+# How to use pivot table directly in mysql?
+
+# Solution 1 - only beat 5%
+
+SELECT t.id,
+SUM(CASE WHEN month="Jan" THEN revenue ELSE NULL END) AS Jan_Revenue,
+SUM(CASE WHEN month="Feb" THEN revenue ELSE NULL END) AS Feb_Revenue,
+SUM(CASE WHEN month="Mar" THEN revenue ELSE NULL END) AS Mar_Revenue,
+SUM(CASE WHEN month="Apr" THEN revenue ELSE NULL END) AS Apr_Revenue,
+SUM(CASE WHEN month="May" THEN revenue ELSE NULL END) AS May_Revenue,
+SUM(CASE WHEN month="Jun" THEN revenue ELSE NULL END) AS Jun_Revenue,
+SUM(CASE WHEN month="Jul" THEN revenue ELSE NULL END) AS Jul_Revenue,
+SUM(CASE WHEN month="Aug" THEN revenue ELSE NULL END) AS Aug_Revenue,
+SUM(CASE WHEN month="Sep" THEN revenue ELSE NULL END) AS Sep_Revenue,
+SUM(CASE WHEN month="Oct" THEN revenue ELSE NULL END) AS Oct_Revenue,
+SUM(CASE WHEN month="Nov" THEN revenue ELSE NULL END) AS Nov_Revenue,
+SUM(CASE WHEN month="Dec" THEN revenue ELSE NULL END) AS Dec_Revenue
+FROM Department t
+GROUP BY t.id
+
+
+# Solution, similar to the previous one
 SELECT t.id,
 SUM(t.Jan_Revenue) AS Jan_Revenue,
 SUM(t.Feb_Revenue) AS Feb_Revenue,
