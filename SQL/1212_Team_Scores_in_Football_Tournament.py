dfc324e555b968a340c4a576858a4a5c22a3d6f8
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-08-12 16:11:22 -0700

    Update 1212_Team_Scores_in_Football_Tournament.py

--- SQL/1212_Team_Scores_in_Football_Tournament.py
+++ SQL/1212_Team_Scores_in_Football_Tournament.py
@@ -1,5 +1,28 @@
 # Write your MySQL query statement below
 
+#Method 0 - beat 20% - IFNULL(SUM(..), 0) - if SUM(..) is NULL, then set it to be 0. (Note: SUM could be in IFNULL function)
+SELECT t.team_id, t.team_name, IFNULL(SUM(s.score), 0) AS num_points
+FROM Teams t
+
+LEFT JOIN
+(SELECT m1.host_team AS team,
+       CASE WHEN host_goals>guest_goals THEN 3
+       WHEN host_goals=guest_goals THEN 1
+       ELSE 0 END AS score
+FROM Matches m1
+
+UNION ALL
+
+SELECT m2.guest_team AS team,
+       CASE WHEN guest_goals>host_goals THEN 3
+       WHEN guest_goals=host_goals THEN 1
+       ELSE 0 END AS score
+FROM Matches m2) s
+
+ON t.team_id=s.team
+GROUP BY t.team_id, t.team_name
+ORDER BY num_points DESC, t.team_id
+
 
 # Method 1 - Use SUM + CASE WHEN to deal with NULL,
 # ELSE 0 END must be at the end, not ELSE 1 END
@@ -23,7 +46,7 @@ GROUP BY t1.team_id, t1.team_name
 ORDER BY num_points DESC, t1.team_id ASC
 
 
-# Method 2 - IFNULL deal with NULL, SUM should be at the inner level, not in IFULL
+# Method 2 - IFNULL deal with NULL, SUM should be at the inner level, not in IFULL: (Note: As proved later, SUM can be in inner level of IFNULL, see Method 0)
 # function
 # IFNULL could not use SUM inside directly
 SELECT t.team_id, t.team_name, IFNULL(t.num_points, 0) AS num_points
